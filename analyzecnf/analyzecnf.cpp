7be110982c57d18f0d2731affdb4b24132ebeac8
tscnn <tobias.schomann@gmail.com>
2014-12-09 22:35:37 +0100

    updated analyzecnf

--- analyzecnf/analyzecnf.cpp
+++ analyzecnf/analyzecnf.cpp
@@ -1,53 +1,67 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 
+#define MAX_K 10
+
 int main(int argc, char **argv) {
-    
+
     //read from file or from stdin
     FILE *fp;
-    if (argc >= 2) {
+    if(argc>=2)
+    {
         fp = fopen(argv[1],"r");
-    } else {
+    }else{
         fp = stdin;
     }
-    
+
     //skip comments
     int c;
     while ((c = fgetc(fp)) == 'c')
         while (fgetc(fp) != '\n') {}
-    
+
     //parse formula
-    if (c == 'p') {
-        int n,m,l,clause[3],p=0,ic=0,c=0;
-        if (fscanf(fp," cnf %d %d\n",&n,&m)!=2) {
-            printf("cannot read input");
-            return 0;
+    if (c == 'p')
+    {
+        int n;
+        int m;
+        int u;
+        int p=0;
+        int c[MAX_K+1]={0};
+        int csum=0;
+
+        if (fscanf(fp," cnf %d %d\n",&n,&m)!=2)
+        {
+            fprintf(stderr,"Error: Can not read header.\n");
+            exit(0);
         }
+
         int* nbvar = new int[n]; memset(nbvar,0,n*sizeof(int));
         int* nbneg = new int[n]; memset(nbneg,0,n*sizeof(int));
-        while (fscanf(fp,"%d",&l) != EOF) {
-            if (l == 0) {
-                c++;
-                if (clause[0]==clause[1] || -clause[0]==clause[1] || 
-                    clause[1]==clause[2] || -clause[1]==clause[2] ||
-                    clause[0]==clause[2] || -clause[0]==clause[2] || p!=3)
-                {
-                    ic++; //illegal clause found
-                }
-                p = 0;
-            } else {
-                clause[p] = l;
+
+        while (fscanf(fp,"%d",&u) != EOF)
+        {
+            if (u==0)
+            {
+                c[p>MAX_K ? MAX_K : p]++;
+                csum++;
+                p=0;
+            }else{
                 p++;
-                if (l > 0) {
-                    nbvar[l-1]++;
-                } else {
-                    nbneg[-l-1]++;
-                }
+                if(u>0) nbvar[u-1]++;
+                else nbneg[-u-1]++;
             }
         }
         printf("\n");
-        printf("number of clauses: %d\n",c);
-        printf("number of illegal clauses: %d\n",ic);
+        printf("number of clauses: %d\n",csum);
+        if(c[0]>0) printf("number of empty clauses: %d\n",c[0]);
+        if(c[1]>0) printf("number of unit clauses: %d\n",c[1]);
+        for(int i=2; i<MAX_K; i++)
+        {
+            if(c[i]>0) printf("number of %d-clauses: %d\n",i,c[i]);
+        }
+        if(c[MAX_K]>0) printf("number of >=%d-clauses: %d\n",MAX_K,c[MAX_K]);
+
         printf("\n");
         printf("   Var          +          -\n");
         printf("============================\n");
@@ -60,6 +74,6 @@ int main(int argc, char **argv) {
         printf("============================\n");
         printf("   Sum %10d %10d\n",sumvar,sumneg);
     }
-    
+
     return 0;
 }
